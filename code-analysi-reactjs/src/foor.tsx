778b4065ae23b98dfd288ebf2be6421700039c2d
Thanh Bui <btnthanh94@gmail.com>
2020-02-27 15:36:30 +0700

    up 5

--- code-analysi-reactjs/src/foor.tsx
+++ code-analysi-reactjs/src/foor.tsx
@@ -1,4 +1,5 @@
 import React, { PureComponent, ReactNode } from 'react'
+import PureComponent1 from './PureComponent1'
 
 interface FoorProps {
 
@@ -26,7 +27,7 @@ class Foor extends PureComponent<FoorProps, FoorState> {
         
       }
     render(): ReactNode {
-    return <div onClick={this.onClickDiv(1, 2)} ><div></div>{[1,2,3,4,5].map(v=><span>{v}</span>)}</div>
+    return <div onClick={this.onClickDiv(1, 2)} ><div></div>{[1,2,3,4,5].map(v=><span>{v}</span>)} <PureComponent1 phone_number="0943443343" />  </div>
     }
 }
 
